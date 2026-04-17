0305d3ec9c2477856636bb1e9ad1640caf272219
rcopra <137560494+rcopra@users.noreply.github.com>
2026-04-17 22:52:21 +0200

    sync

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pygame
 from constants import SCREEN_WIDTH
 from constants import SCREEN_HEIGHT
 from logger import log_state
+from player import Player
 
 
 def main():
@@ -10,11 +11,17 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    dt = 0
     while True:
+        dt = clock.tick(60) / 1000
         log_state()
-        for even in pygame.event.get():
+        for event in pygame.event.get():
             pass
         screen.fill("black")
+        player.update(dt)
+        player.draw(screen)
         pygame.display.flip()
 
 if __name__ == "__main__":
